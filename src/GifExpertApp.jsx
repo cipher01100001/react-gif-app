54d59dc114678bf5e9cf44d5cacd95144ca437b5
cipher <cipher01100001@gmail.com>
2022-10-29 15:47:13 -0500

    first commit to deployment

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { AddCategory, GifGrid } from './components';
+
+export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState([]);
+
+  const onAddCategory = (newCategory) => {
+    if (categories.some(category => category.toLowerCase() === newCategory.toLowerCase())) return;
+    setCategories(cat => [newCategory, ...cat]);
+  };
+
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+
+      <AddCategory
+        onNewCategory={onAddCategory}
+      />
+
+      {
+        categories.map(category => (
+          <GifGrid
+            key={category}
+            category={category}
+          />
+        ))
+      }
+    </>
+  )
+}
